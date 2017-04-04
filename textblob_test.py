c7801172c3617a2a00e5ac74d4a7af0cda980070
Thomas Bagley <thb7@duke.edu>
2017-04-04 15:30:03 -0400

    Updated to write to CSV file

--- textblob_test.py
+++ textblob_test.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
-
+import csv
 from textblob import TextBlob
 
-def analyze_file(file_name):
-	file = open(file_name,'r')
-	sentences = []
-	sentence = ""
-	for line in file:
-		utf_line = line.decode("utf-8")
-		ascii_line  =utf_line.encode("ascii","ignore")
-		if 'ARTICLE' in ascii_line:
-			sentences.append(sentence)
-			sentence = ""
-		else:
-			sentence+= " " + ascii_line
-	for sentence in sentences:
-		if len(sentence) > 0:
-			print sentence[0:100]
-			blob = TextBlob(sentence)
-			print blob.sentiment
-			print "\n"
+def analyze_file(file_name, mode):
+	with open('results.csv',mode) as csvfile:
+		data_file = open(file_name,'r')
+		sentences = []
+		sentence = ""
+		csv_writer = csv.writer(csvfile, delimiter=',', lineterminator = '\n')
+		extension_pos = file_name.index('.')
+		raw_file_name = file_name[:extension_pos]
+		csv_writer.writerow([raw_file_name.upper(), raw_file_name.upper()])
+		csv_writer.writerow(["Polarity","Subjectivity"])
+		for line in data_file:
+			utf_line = line.decode("utf-8")
+			ascii_line  =utf_line.encode("ascii","ignore")
+			if 'ARTICLE' in ascii_line:
+				sentences.append(sentence)
+				sentence = ""
+			else:
+				sentence+= " " + ascii_line
+		for sentence in sentences:
+			if len(sentence) > 0:
+				#print sentence[0:100]
+				blob = TextBlob(sentence)
+				(polarity, subjectivity) = blob.sentiment
+				csv_writer.writerow([polarity, subjectivity])
+				#print "\n"
+		csv_writer.writerow(['',''])
 
 def analyze_literal(sentences):
 	for sentence in sentences:
@@ -35,7 +43,13 @@ if __name__=="__main__":
 	analyze_literal(sentences)
 	print "-------------FILES----------------------\n"
 	file_names = ['NYTArticles.txt','NYTBlogArticles.txt']
+	first = True
 	for file_name in file_names:
 		print file_name
-		analyze_file(file_name)
+		if first:
+			analyze_file(file_name, 'w')
+			first = False
+		else:
+			analyze_file(file_name, 'a')
+		
 		
