b35e4bd2270c652261b233ce3e266c0c638e84c1
Thomas Bagley <thb7@duke.edu>
2017-04-21 00:09:10 -0400

    Refactoring. Updated csv is my preferred final format for results - should be easy to read and visualize off of

--- Analysis/source_analysis.py
+++ Analysis/source_analysis.py
@@ -3,6 +3,7 @@ import csv
 from textblob import TextBlob
 
 LEN_ROWS = 110
+SUMMARY_STATS = 3
 #Removes extension and writes filename in upper case for write_csv_header
 def process_file_name(file_name):
     name_start = max(file_name.rfind('\\') , file_name.rfind('/'))
@@ -26,15 +27,14 @@ def compute_median(vals):
         return vals[middle] #Truncates to an int
         
 #Performs sentiment analysis on a text file containing some number of articles 
-def analyze_file(file_name, mode):
+def analyze_file(file_name):
     
     data_file = open(file_name,'r')
     sentences = []
     subjectivities = []
     polarities = []
     sentence = ""
-   
-   
+      
     for line in data_file:
         ascii_line = ascii_prepare(line)
         if 'ARTICLE' in ascii_line:
@@ -51,36 +51,53 @@ def analyze_file(file_name, mode):
            
     return (subjectivities,polarities)
 
+def compute_summary(subjectivities, polarities):
+    mean_row = ['mean',compute_mean(subjectivities), compute_mean(polarities)]
+    abs_mean_row = ['Absolute mean', compute_mean(subjectivities),compute_mean([abs(p) for p in polarities])]
+    median_row = ['median', compute_median(subjectivities), compute_median(polarities)]
+    return [mean_row, abs_mean_row, median_row]
+
 def process_files(csv_writer, file_names, title):
         rows = [[] for i in range(LEN_ROWS)]
         csv_writer.writerow(['',title.upper(), ''])
         all_subj = []
         all_pol = []
         for file_name in file_names:
+            #Prepare header
             rows[0].extend(['',process_file_name(file_name),''])
             rows[1].extend(["","Subjectivity", "Polarity"]) 
-            (subjectivities, polarities) = analyze_file(file_name, 'w') #Overwrites any old versions
+            
+            #Compute sentiment analysis
+            (subjectivities, polarities) = analyze_file(file_name) 
+            
             all_subj.extend(subjectivities)
             all_pol.extend(polarities)
-            for i in range(2,min(len(rows)-3,2+len(subjectivities))):
+            
+            #Prepare to write to file
+            for i in range(2,min(LEN_ROWS-SUMMARY_STATS,2+len(subjectivities))):
                 rows[i].extend(["", subjectivities[i-2], polarities[i-2]])
-            for j in range(i+1, len(rows)-3):
+            for j in range(i+1, LEN_ROWS-SUMMARY_STATS):
                 rows[j].extend(['','',''])
-            rows[LEN_ROWS-3].extend(['mean',compute_mean(subjectivities), compute_mean(polarities)])
-            rows[LEN_ROWS-2].extend(['Absolute mean', compute_mean(subjectivities),compute_mean([abs(p) for p in polarities])])
-            rows[LEN_ROWS -1].extend(['median', compute_median(subjectivities), compute_median(polarities)])
+            
+            #Prepare summary statistics to write to file
+            summary_rows = compute_summary(subjectivities, polarities)
+            for i in range(len(summary_rows)):
+                rows[LEN_ROWS+(i-3)].extend(summary_rows[i])
+                
             for row in rows:
                 row.append('') #Spacing 
+                
+        rows.append([])
+        spaces = [' '] * ((4 * len(file_names) - 4) / 2) #Centering for the overall summary
+        rows.append(spaces + ['', title + ' Summary'])
+        summary_rows = compute_summary(all_subj, all_pol)
+        for row in summary_rows:
+            rows.append(spaces + row)
+
+        rows.append([])
+
         for row in rows:
             csv_writer.writerow(row)
-        spaces = [' ']*((4*len(file_names)-4)/2)
-        csv_writer.writerow([])
-        
-        csv_writer.writerow(spaces + ['', title + ' Summary'])
-        csv_writer.writerow(spaces + ['overall mean',compute_mean(all_subj), compute_mean(all_pol)] )
-        csv_writer.writerow(spaces + ['absolute mean', compute_mean(all_subj),compute_mean([abs(p) for p in all_pol])]) #Subjectivity is 0-1
-        csv_writer.writerow(spaces + ['median', compute_median(all_subj), compute_median(all_pol)])
-        csv_writer.writerow([])
         
 
 if __name__=="__main__":
